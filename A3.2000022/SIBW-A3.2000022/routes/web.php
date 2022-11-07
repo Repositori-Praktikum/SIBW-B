2b80d0706d1f2aadea81d13bd0949c203fb011fe
Gilang1225 <gilanggumelarramadan7@gmail.com>
2022-11-07 11:48:48 +0700

    Gilang Gumelar Ramadan - UTS

--- A3.2000022/SIBW-A3.2000022/routes/web.php
+++ A3.2000022/SIBW-A3.2000022/routes/web.php
@@ -17,4 +17,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('blog', BlogController::class);
+Route::resource('/blog', \App\Http\Controllers\BlogController::class);
